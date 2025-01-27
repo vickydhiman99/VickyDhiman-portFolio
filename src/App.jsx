1bd9faeb3de27effcc83415441f7735edc18d690
Vicky Kumar <140242560+vickydhiman99@users.noreply.github.com>
2025-01-27 12:34:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>hello vicky</h1>
-      <br />
-      <h2>all okay</h2>
-      <h3>all the best</h3>
+      <h1>Work in progress.....</h1>
     </div>
   )
 }
